046e37ffaf6ec0582310b1fe3983c3d15a90e3f7
IMTIYAZ M SHIRASANGI <117428054+imtiyaz-github@users.noreply.github.com>
2023-06-07 23:55:02 +0530

    integrating redux with react

--- src/components/redux-demo.js
+++ src/components/redux-demo.js
@@ -1,29 +1,28 @@
+const redux = require('redux');
 
-const redux = require("redux");
-
-const counterReducer = (state = { counter: 0 }, action) => {
-  if (action.type === "increment") {
-    return {
-      counter: state.counter + 5,
-    };
-  }
-
-  if (action.type === "decrement") {
-    return {
-      counter: state.counter - 1,
-    };
-  }
+const counterReducer=(state = { counter : 0}, action)=>{
+    if(action.type==='INCREMENTBY2'){
+        return {
+            counter:state.counter + 2
+        };
+    }
+    if(action.type==='DECREMENTBY2'){
+        return {
+            counter:state.counter-2
+        }
+    }
+    
 };
 
 const store = redux.createStore(counterReducer);
 console.log(store.getState());
 
-console.log(counterReducer);
-const counterSubscriber = () => {
-  const latestState = store.getState();
-  console.log(latestState);
-};
+const counterSubscriber=()=>{
+    const latestState=store.getState();
+    console.log(latestState)
+}
 
-store.subscribe(counterSubscriber);
-store.dispatch({ type: "increment" });
-store.dispatch({ type: "decrement" });
+store.subscribe(counterSubscriber)
+
+store.dispatch({ type:'INCREMENTBY2' })
+store.dispatch({type:'DECREMENTBY2'})
